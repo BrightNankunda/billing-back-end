e1bb49eb9ca2698c1b6616e163008eb6529c4e73
BrightNankunda <67958069+BrightNankunda@users.noreply.github.com>
2021-10-05 17:24:37 +0300

    Notes commit

--- Controllers/NotesController.js
+++ Controllers/NotesController.js
@@ -56,3 +56,30 @@ exports.deleteANote = async (req, res) => {
       res.status(404).json(error.message)
    }
 }
+exports.getAllUserNotes = async (req, res) => {
+   try {
+      const notes = await Note.find({createdBy: req.user.id})
+      console.log(notes)
+      res.json(notes)
+   } catch(err) {
+      console.log(err)
+      res.status(400).json(err)
+   }
+}
+
+exports.postANewNote = async (req, res) => {
+   const createdBy = req.user.id
+
+   const {
+      noteHeader, 
+      noteBody, 
+   } = req.body
+   try {
+      const note = new Note({noteHeader, noteBody, createdBy})
+      const newNote = await note.save()
+      res.status(201).json(newNote)
+   } catch(err) {
+      console.log(err)
+      res.json(err)
+   }
+}
